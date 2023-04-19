c7887101fdfa726a5352de2da1ee7ce3061a9dca
RAVI-ALGO <kushwahark724@gmail.com>
2023-04-20 00:05:49 +0530

    change key

--- models/user.js
+++ models/user.js
@@ -7,7 +7,7 @@ const userSchema = new mongoose.Schema({
 });
 
 
-userSchema.plugin(encrypt ,{ secret:process.env.SECRET , encryptedFields:['password']});
+userSchema.plugin(encrypt ,{ secret:process.env.ENCRYPTION_KEY , encryptedFields:['password']});
 
 
 
